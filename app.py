2dae1fa2a1896a9bd353e1223a1ba4ae32696b7d
Caio Vicente <144162954+Caio-Rodrigues-V@users.noreply.github.com>
2023-10-03 16:48:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,30 +13,11 @@
 
 3 - repetir isso para outro contato """
 
-
-""" import webbrowser as wb
-import pyautogui as py
-from time import sleep
-
-telefones = [5521984354821,553196402751,5521965605687]
-
-for telefone in telefones:
-    wb.open(f'https://api.whatsapp.com/send?phone={telefone}')
-    sleep(10)
-    iniciar = py.locateCenterOnScreen('inciar_conversa.png',confidence=0.9)
-    py.click(iniciar[0],iniciar[1])
-    sleep(10)
-    web = py.locateCenterOnScreen('web.png',confidence=0.9)
-    py.click(web[0],web[1])
-    py.typewrite("ASDAD")
-    py.press('enter')
-    sleep(200) """
-
-
 import webbrowser as wb
 import pyautogui as py
 from time import sleep
 
+#caso queira usar crie um txt chamado fones.txt na mesma pasta do seu projeto e la dentro coloque os contatos que você quer interagir
 telefones = []
 with open('fones.txt','r') as arquivo:
     for linha in arquivo:
@@ -52,6 +33,7 @@ for telefone in telefones:
     web = py.locateCenterOnScreen('web.png',confidence=0.9)
     py.click(web[0],web[1])
     sleep(15)
+   #Altere o "Olá" para a mensagem que você deseja enviar
     py.typewrite("Olá ! !")
     py.press('enter')
     sleep(200)
