20139a1b4d4bc593ab625da001e147273942d523
Caio Vicente <144162954+Caio-Rodrigues-V@users.noreply.github.com>
2023-10-03 14:28:42 -0300

    mini automação para guitar flash

--- app.py
+++ app.py
@@ -1,67 +1,24 @@
-#Gerador de dados aleatorios
+#entender como funciona o bot - passos manuais
 
-import random
 
-#função para gerar nomes aleatorios
-def gerar_nome():
-    nomes = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
-    return random.choice(nomes)
+# 1 -  reconhecer cada teclado do jogo (posso usar cores rgb )
 
-#função para gerar telefones aleatorios
-def gerar_telefones():
-    telefones = ['55219854', '552154485', '55421156', '5521556', '55212525']
-    return random.choice(telefones)
+# 2 - achar um jeito de quando a tecla do jogo passar encima da tecla de botoes ele acionar a tecla e pontuar
 
-#função pra gerar cidades aleatorias
-def gerar_cidades():
-    cidades = ['rio de janeiro', 'olinda', 'varginha', 'bh', 'bs']
-    return random.choice(cidades)
+import pyautogui as py
+import keyboard as ky
+from time import sleep
 
-#funçaõ para gerar estados aleatorios
-def gerar_estados():
-    estados = ['RJ', 'PE', 'CE', 'BA', 'MG']
-    return random.choice(estados)
 
-#função para gerar emails aleatorios
-def gerar_email():
-    emails = ['caiovicenterj@gmail.com', 'oiasdoias@gmail.com', '22222222222@gmail.com', '3333333333333@gmail.com', '44444444444444@gmail.com']
-    return random.choice(emails)
+while ky.is_pressed('1') == False:
 
-#dicionario para salvar as opçoes correspondestes
-opcoes = {
-    '1': gerar_nome,
-    '2': gerar_telefones,
-    '3': gerar_cidades,
-    '4': gerar_estados,
-    '5': gerar_email
-}
-
-#Váriavel para controlar se os dados devem ser salvos ou nao
-salvar_arquivo = False
-
-while True:
-    print("Escolha uma opção:")
-    print("1. Gerar nome")
-    print("2. Gerar e-mail")
-    print("3. Gerar telefone")
-    print("4. Gerar cidade")
-    print("5. Gerar estado")
-    print("6. Parar")
-
-    opcao_digitadas = input("Digite um numero da opção desejada: ")
-    opcao_selecionada = opcao_digitadas.split(',')
+    if py.pixelMatchesColor(811,793,(0,152,0)):
+        py.press('a')
     
 
-    if '6' in opcao_selecionada:
-        break
+    if py.pixelMatchesColor(878,793,(255,0,0)):
+        py.press('s')
+    
 
-
-    with open('dados.txt', 'a') as arquivo:  # Abre o arquivo uma vez em modo de adição ('a')
-        for opcao in opcao_selecionada:
-            if opcao in opcoes:
-                dado_gerado = opcoes[opcao]()
-                print(f"Dado gerado: {dado_gerado}")
-                arquivo.write(f"{dado_gerado}\n") 
-                
-if salvar_arquivo:
-    print("Dados salvo em .txt")
+    if py.pixelMatchesColor(970,794,(244,244,2)):
+        py.press('j')
